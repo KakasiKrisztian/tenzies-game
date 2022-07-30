3083f144f0c63593c5b5e3897fff716998ed55a2
KakasiKrisztian <54312225+KakasiKrisztian@users.noreply.github.com>
2022-07-30 21:56:40 +0300

    First commit
    First functional version of the game, comprised of the working game and tracking the best score

---
+++ tenzies-game/App.js
@@ -0,0 +1,100 @@
+import React from "react"
+import Die from "./Die"
+import {nanoid} from "nanoid"
+import Confetti from "react-confetti"
+
+export default function App() {
+
+    const [dice, setDice] = React.useState(allNewDice())
+    const [tenzies, setTenzies] = React.useState(false)
+    const [stats, setStats] = React.useState({
+        rolls: 0,
+        bestRolls: JSON.parse(localStorage.getItem("rolls")) || 0
+    })
+    
+    React.useEffect(() => {
+        const allHeld = dice.every(die => die.isHeld)
+        const firstValue = dice[0].value
+        const allSameValue = dice.every(die => die.value === firstValue)
+        if (allHeld && allSameValue) {
+            setTenzies(true)
+            let bestScore = JSON.parse(localStorage.getItem("rolls"))  || 0
+            let currentScore = stats.rolls
+            if(currentScore < bestScore || !bestScore){
+                console.log("You beat the record!")
+                localStorage.setItem("rolls", JSON.stringify(currentScore))
+                setStats(prevStats => ({...prevStats, bestRolls: currentScore}))
+            }else{
+                console.log("Current record is " + bestScore)
+            }
+        }
+    }, [dice])
+
+    function generateNewDie() {
+        return {
+            value: Math.ceil(Math.random() * 6),
+            isHeld: false,
+            id: nanoid()
+        }
+    }
+    
+    function allNewDice() {
+        const newDice = []
+        for (let i = 0; i < 10; i++) {
+            newDice.push(generateNewDie())
+        }
+        return newDice
+    }
+    
+    function rollDice() {
+        if(!tenzies) {
+            setStats(prevStats => ({...prevStats, rolls: ++prevStats.rolls}))
+            setDice(oldDice => oldDice.map(die => {
+                return die.isHeld ? 
+                    die :
+                    generateNewDie()
+            }))
+        } else {
+            setTenzies(false)
+            setDice(allNewDice())
+            setStats(prevStats => ({...prevStats, rolls: 0}))
+        }
+    }
+    
+    function holdDice(id) {
+        setDice(oldDice => oldDice.map(die => {
+            return die.id === id ? 
+                {...die, isHeld: !die.isHeld} :
+                die
+        }))
+    }
+    
+    const diceElements = dice.map(die => (
+        <Die 
+            key={die.id} 
+            value={die.value} 
+            isHeld={die.isHeld} 
+            holdDice={() => holdDice(die.id)}
+        />
+    ))
+    
+    return (
+        <main>
+            {tenzies && <Confetti />}
+            <h1 className="title">Tenzies</h1>
+            <p className="instructions">Roll until all dice are the same. 
+            Click each die to freeze it at its current value between rolls.</p>
+            {tenzies && <h3 className="stats-rolls">Last try number of rolls: {stats.rolls} rolls</h3>}
+            {tenzies && <h3 className="stats-time">Current supreme best score ever: {stats.bestRolls} rolls</h3>}
+            <div className="dice-container">
+                {diceElements}
+            </div>
+            <button 
+                className="roll-dice" 
+                onClick={rollDice}
+            >
+                {tenzies ? "New Game" : "Roll"}
+            </button>
+        </main>
+    )
+}
